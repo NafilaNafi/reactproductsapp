0ee42ac26f6605d5e2b41661799baac11c42a79f
NafilaNafi <Nafila@outlook.com>
2020-03-13 11:11:59 +0530

    dummy contact page

---
+++ src/pages/contact.js
@@ -0,0 +1,39 @@
+import React from "react"
+import { graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import HeroSection from "../components/Reuseable/HeroSection"
+import Infoblock from "../components/Reuseable/Infoblock"
+import Dualinfoblock from "../components/Reuseable/Dualinfoblock"
+import Teamservice from "../components/About/Teamservice"
+
+const AboutPage = ({ data }) => (
+    <Layout>
+        <SEO title="Home" />
+
+        <HeroSection
+            img={data.img.childImageSharp.fluid}
+            title="About Iscriptio"
+            subtitle=""
+            heroclass="about-background"
+        />
+        <Dualinfoblock heading="Why We Are" />
+        <Infoblock heading="About Vision" />
+        <Teamservice />
+    </Layout>
+)
+
+export const query = graphql`
+  {
+    img: file(relativePath: { eq: "about.png" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+  }
+`
+
+export default AboutPage
